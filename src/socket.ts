6f84186b8a688a19108b3b29ec1af3e408bdd817
Abh-I-sek <abhisek211004@gmail.com>
2024-11-27 15:16:55 +0530

    added conn close logic

--- src/socket.ts
+++ src/socket.ts
@@ -1,30 +1,38 @@
-import WebSocket from "ws";
-import { PubSubManager } from "./PubSubManager";
+import WebSocket from "ws"
+import { PubSubManager } from "./PubSubManager"
 
+const server1 = new WebSocket.Server({ port: 3000 })
+server1.on("connection", (socket) => {
+  console.log("Client Connected to Server1")
+  socket.on("message", (msg) => {
+    console.log("Server1 recieved : " + msg)
+    PubSubManager.getInstance().addUser(socket, msg.toString())
+  })
 
-const server1 = new WebSocket.Server({port:3000})
-server1.on('connection',(socket)=>{
-    console.log("Client Connected to Server1");
-    socket.on('message',(msg)=>{
-        console.log("Server1 recieved : " + msg);
-        PubSubManager.getInstance().addUser(socket,msg.toString());
-    })
+  socket.on("close", () => {
+    socket.send("Disconnected !")
+    // Here I hard coded the stock ticker which I have to remove
+    // but actually should take the input from user in the msg as a json or something then do it
+    PubSubManager.getInstance().removeUser(socket, "APPLE")
+  })
 })
 
+const server2 = new WebSocket.Server({ port: 3001 })
+server2.on("connection", (socket) => {
+  console.log("Client Connected to Server2")
+  socket.on("message", (msg) => {
+    console.log("Server2 recieved : " + msg)
+    PubSubManager.getInstance().addUser(socket, msg.toString())
+  })
 
-
-const server2 = new WebSocket.Server({port:3001})
-server2.on('connection',(socket)=>{
-    console.log("Client Connected to Server2");
-    socket.on('message',(msg)=>{
-        console.log("Server2 recieved : " + msg);
-        PubSubManager.getInstance().addUser(socket,msg.toString());
-    })
+  socket.on("close", () => {
+    socket.send("Disconnected !")
+    // Here I hard coded the stock ticker which I have to remove
+    // but actually should take the input from user in the msg as a json or something then do it
+    PubSubManager.getInstance().removeUser(socket, "APPLE")
+  })
 })
 
 /*
-
-    1 -> Add logic for when a client leaves the socket connection
-    2 -> And make the msg come as json and then decide whether to subscribe or unsubscribe 
-
+    1 -> And make the msg come as json and then decide whether to subscribe or unsubscribe 
 */
